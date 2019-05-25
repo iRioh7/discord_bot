f6f4d5541841359b6b63b057f1c39aed0bf39873
iRioh7 <rioh777@gmail.com>
2019-05-25 10:04:35 -0500

    Cleaned up print statements

--- bots/common.py
+++ bots/common.py
@@ -6,10 +6,8 @@ import random
 def dice_roll( message ):
     rand_int = [ ]
     requested_dice = message.split( 'd' )
-    print( requested_dice )
     dices = int( requested_dice[ 0 ] )
     dice_type = int( requested_dice[ 1 ] )
-    print( dices, dice_type )
 
     for d in range( dices ):
         rand_int.append( random.randint( 0, dice_type ) )
